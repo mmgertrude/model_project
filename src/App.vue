9adadec683b8737ffabcee9ff163bfe89b1f7155
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2021-03-15 21:27:52 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h1>{{title}}</h1>
+  <p>welcome...</p>
+  <!--
+  <input type="text" ref="refName">
+  <button @click="handleClick">click me</button>
+  -->
+  <!--demo: use of props.To pass data to Model component
+  add the prop attribute header and assign it a name "somestring"
+  The string is passed to the Model componentas a prop named header
+  <div v-if="showModel"> 
+  <Model :header="header" :text="text" theme="sale" @closeThis="toggleModel"/>
+  -->
+    <div v-if="showModel"> 
+      <Model theme="" @closeThis="toggleModel">
+        <template v-slot:links>
+          <a href="#"> signup now </a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja giveaway</h1>
+        <p>Grab your favourite coffee for half price!</p>
+        </Model>
+    </div>
+  
 
+<!--- challenge -->
+ <div v-if="showModelTwo"> 
+    <Model @closeThis="toggleModelTwo">
+      
+      <h1>Sign up for our newsletter</h1>
+      <p>for updates and promo codes</p>
+    </Model>
+
+  </div>
+  <button @click.alt="toggleModel"> open model (opt)</button>
+  <button @click="toggleModelTwo"> open model two</button>
+</template>
+<!-- end of challenge -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Model from "./components/Model.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'App', //name of component
+  components: {Model}, //register child component
+  data(){
+    return{
+      title:  "my first vue app :)",
+      showModel: false,
+      showModelTwo: false
+      }
+  },
+  methods:{
+    toggleModel(){
+      this.showModel = !this.showModel
+    },
+    toggleModelTwo(){
+      this.showModelTwo = !this.showModelTwo
+    }
   }
 }
 </script>
@@ -22,5 +68,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Model.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="backdrop" @click.self="closeModel">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <!--
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+            -->
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>        
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    //list/register props that Model.vue will be accepting
+    //props: ["header", "text", "theme"],
+    props: ["theme"],
+    methods: {
+        closeModel(){
+            this.$emit("closeThis")
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal h1{
+color: #03cfb4;
+border: none;
+padding: 0;
+}
+.modal p {
+  font-style: normal;  
+}
+.model .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+
+}
+
+.modal.sale{
+    background: crimson;
+    color: white;
+}
+.modal.sale h1{
+    color: white;
+}
+.modal.sale .actions{
+    color: white;
+}
+.modal.sale .actions a{
+    color: white;
+}
+</style>
